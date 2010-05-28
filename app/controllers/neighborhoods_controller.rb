47890b041f547fc9f7bff2ee0d5f7d6964c0a1ec
Leo Herrick <leoherrick@gmail.com>
2010-05-28 11:53:54 -0700

    added ajax onclick to hoods 2

---
+++ app/controllers/neighborhoods_controller.rb
@@ -0,0 +1,9 @@
+class NeighborhoodsController < ActionController::Base
+  protect_from_forgery
+  
+  def show
+    @neighborhood = Neighborhood.find(params[:id])
+
+    render :json => @neighborhood.to_json
+  end
+end
