fd3ac6cbe7c27f6f99b36ae868a2097039e6ed22
Leo Herrick <leoherrick@gmail.com>
2010-10-26 20:47:25 -0700

    removed auth again... cleaned up messages and garden page for staging

--- app/controllers/my_garden_controller.rb
+++ app/controllers/my_garden_controller.rb
@@ -1,7 +1,8 @@
 class MyGardenController < ApplicationController
+#  before_filter :authenticate_user!
   
   def index
-    @crops = current_user.crops
+#    @crops = current_user.crops
   end
   
 end
